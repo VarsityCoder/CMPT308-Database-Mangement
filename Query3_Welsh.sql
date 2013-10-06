8335599c669a7b3381f42d97d23185be82f99c92
Miles "The Boss" Welsh <miles.welsh1@marist.edu>
2013-10-06 19:11:44 -0400

    This is my first commit but I have a long way to go I think the first 3 are right but I have some work to do tonight

---
+++ Query3_Welsh.sql
@@ -0,0 +1,28 @@
+-- 1
+select city 
+from agents 
+WHERE aid in (
+ select aid 
+ FROM orders 
+ WHERE cid = 'c002');
+ 
+-- 2
+select city 
+from agents
+join orders on agents.aid = orders.aid
+where cid = 'c002'; 
+
+-- 3
+select  distinct pid
+from orders
+where aid in (select aid
+from orders
+where cid in (select cid
+from customers
+where city = 'Kyoto'))
+ 
+-- 4
+select distinct pid
+from orders
+join orders on customers.cid = order.aid
+where city = 'Kyoto';
