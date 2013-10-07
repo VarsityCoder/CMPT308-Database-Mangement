de8f7282ab5ea334a3e22468bbbb43a9c870a675
Miles "The Boss" Welsh <miles.welsh1@marist.edu>
2013-10-06 20:00:16 -0400

    I got 5 done but I still cant figure out 4. Yeah for progress!

--- Query3_Welsh.sql
+++ Query3_Welsh.sql
@@ -13,7 +13,7 @@ join orders on agents.aid = orders.aid
 where cid = 'c002'; 
 
 -- 3
-select  distinct pid
+select distinct pid
 from orders
 where aid in (select aid
 from orders
@@ -26,3 +26,9 @@ select distinct pid
 from orders
 join orders on customers.cid = order.aid
 where city = 'Kyoto';
+
+--5
+select name 
+from customers 
+where cid not in (
+select cid from orders)
